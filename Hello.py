17a8224438219ef6b8ac63ba5b1813943e1a59bb
Asmiya <mailtomeisasmiya@gmail.com>
2026-06-28 17:39:52 +0530

    Data types in python

--- Hello.py
+++ Hello.py
@@ -1,5 +1,85 @@
 print("Hello World")
 print("Welcome to python")
 
+#Integers with airthmatic oprtaion direclty 
+print(12+30)
+print(".................")
+#String
 
+name = "Asmiya"
+city = "Hyderabad"
+
+print(name)
+print(city)
+print(".................")
+
+#FLoating
+
+weight = 19.25
+
+print(weight)
+
+print(".................")
+#Boolen
+
+is_employee = True
+is_admin = False
+
+print(is_employee)
+print(is_admin)
+
+print(".................")
+print(".................")
+
+#List:  A list stores multiple items in order.
+
+
+
+skills = ["Python", "Salesforce", "Git", "AI", "LWC","Apex"]
+
+print(skills)
+print(skills[0])
+
+
+print(".................")
+print(".................")
+
+
+#Truble  A tuple is like a list, but its contents cannot be changed.
+
+months = ("Jan", "Feb", "Mar","April","May")
+
+print(months)
+
+print(months[3])
+
+print(".................")
+print(".................")
+
+
+#Dictionary: A dictionary stores data using keys and values.
+
+
+
+employee = {
+    "name": "Asmiya",
+    "age": 31,
+    "role": "Salesforce FIeld service consultant"
+}
+
+print(employee)
+print(employee["name"])
+
+
+print(".................")
+print(".................")
+print(".................")
+print(".................")
+
+
+#Set : A set stores unique values only (no duplicates).
+
+numbers = {1, 2, 3, 4, 4, 5}
+
+print(numbers)
 
